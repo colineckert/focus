8707b8107cf28601ee1faa40e6a6b07e47a16a50
Colin Eckert <colineckert10@gmail.com>
2025-05-17 15:18:51 -0700

    Add basic CLI structure for Pomodoro timer app
    Co-authored-by: Colin Eckert <colineckert10@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,24 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "focus",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A simple CLI Pomodoro timer",
+	Long: `Focus is a minimal command-line Pomodoro timer
+to help you stay productive in timed sprints.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to Focus! Use 'focus start' to begin a Pomodoro session.")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +41,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 Colin Eckert <colineckert10@gmail.com>
-
 */
 package main
 
